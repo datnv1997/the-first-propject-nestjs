8ecf0248328a85b49fcb8553ee36d60b5d0dba0c
Nguyen Dat <dat.nguyen7@ntq-solution.com.vn>
2021-06-07 17:58:19 +0700

    update

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,27 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    
+    try {
+      await user.save();
+    } catch (error) {
+      console.log(error.code);
+      
+      if(error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
